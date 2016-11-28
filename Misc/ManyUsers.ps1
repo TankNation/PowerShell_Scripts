a0687bb29ae049c2c2bef1ad68badcc1e3e310e8
TankNation <tanknospam@gmail.com>
2016-11-28 13:33:12 -0700

    Add files via upload

---
+++ Misc/ManyUsers.ps1
@@ -0,0 +1,15 @@
+﻿
+
+$userWinNT = ((wmic computersystem get username /format:list | Out-String).Trim()).split('=')[1].split(' ') -replace "\W", '/'
+Write-Host $userWinNT
+$userFull = ((wmic computersystem get username /format:list | Out-String).Trim()).split('=')[1].split(' ')
+Write-Host $userFull
+$user = ((wmic computersystem get username /format:list | Out-String).Trim()).split('\')[1].split(' ')
+Write-Host $user
+
+
+$userOld = ([Security.Principal.WindowsIdentity]::GetCurrent()).Name.Replace("$env:UserDomain\","") # Doesn't work via Task Sequence
+write-host $userOld
+$domain = ([Security.Principal.WindowsIdentity]::GetCurrent()).Name.Replace("\$env:UserName","") # Doesn't work via Task Sequence
+Write-Host $domain
+#([adsi]”WinNT://./Hyper-V Administrators,group”).Add(“WinNT://$domain/$userOld,user”)
