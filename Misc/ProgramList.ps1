2c6dc944b46d90e3338177b49d92310a0f1fcf3f
Brian T <tanknospam@gmail.com>
2016-12-08 16:08:07 -0700

    Add files via upload

---
+++ Misc/ProgramList.ps1
@@ -0,0 +1,4 @@
+﻿Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select DisplayName, DisplayVersion, Publisher, InstallDate, HelpLink, UninstallString | Out-GridView
+Write-Host "Press any key to continue ..."
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
