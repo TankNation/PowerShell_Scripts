3a548b9845fb27c10805c04d585fe53d767397a1
TankNation <tanknospam@gmail.com>
2016-12-08 15:29:51 -0700

    Add files via upload

---
+++ Hyper-V/Install and Initial Configuration/01_Hyper-V - Feature_Install.ps1
@@ -0,0 +1,31 @@
+﻿#############################################################################################################################################
+###~ Hyper-V - Feature Install ##############################################################################################################
+#
+#~ Brian Tancredi
+#~ Created: 2016-09-27
+#~ Modified: 2016-11-09
+#
+#~ References:
+#~ https://technet.microsoft.com/en-us/library/hh846766.aspx
+#
+#############################################################################################################################################
+
+#############################################################################################################################################
+###~ Installing Hyper-V Role and Features ###################################################################################################
+#############################################################################################################################################
+
+#~ Add user as local Hyper-V Administrator
+$userWinNT = ((wmic computersystem get username /format:list | Out-String).Trim()).split('=')[1].split(' ') -replace "\W", '/'
+([adsi]”WinNT://./Hyper-V Administrators,group”).Add(“WinNT://$userWinNT,user”)
+
+#~ Add and Install All Hyper-V Features on Client OS
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
+
+#~ Add and Install Some But not All Hyper-V Features on Client OS
+#Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V,Microsoft-Hyper-V-Tools-All -All -NoRestart;Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Services -NoRestart
+
+#~ RESTART NEEDED TO PROCEED WITH .\02_HyperV_Initial_Config.ps1
+
+#############################################################################################################################################
+#~ END ######################################################################################################################################
+#############################################################################################################################################
---
+++ Hyper-V/Install and Initial Configuration/02_Hyper-V - Initial_Config.ps1
@@ -0,0 +1,40 @@
+﻿#############################################################################################################################################
+###~ Hyper-V - Initial Configurations #######################################################################################################
+#
+#~ Brian Tancredi
+#~ Created: 2016-09-27
+#~ Modified: 2016-11-07
+#
+#~ References:
+#~ https://blogs.technet.microsoft.com/heyscriptingguy/2013/10/09/use-powershell-to-create-virtual-switches/
+#
+#############################################################################################################################################
+
+#############################################################################################################################################
+###~ Assigning Variables  ###################################################################################################################
+#############################################################################################################################################
+
+#~ Import Hyper-V Module
+Import-Module Hyper-V
+
+#~ Define Variables
+$exNet = Get-NetAdapter -Name 'Ethernet'
+$def_vmPath = "$env:SystemDrive\Hyper-V\Virtual Machines"
+$def_vdPath = "$env:SystemDrive\Hyper-V\Virtual Hard Disks"
+
+#############################################################################################################################################
+###~ Configure Hyper-V Settings #############################################################################################################
+#############################################################################################################################################
+
+#~ Get local adapter name and create Virtual Switch Types within Hyper-V 
+New-VMSwitch -Name "External Virtual Switch" -AllowManagementOS $True -NetAdapterName $exNet.Name -Notes 'WAN - Access to the Internet'
+New-VMSwitch -Name "Private Virtual Switch" -SwitchType Private -Notes ‘Private Network - Internal VMs Only’
+New-VMSwitch -Name "Internal Virtual Switch" -SwitchType Internal -Notes ‘Internal Network - Host OS and Internal VMs Only’
+
+#~ Adjust VM and VHD Default Folders
+Set-VMHost -VirtualMachinePath $def_vmPath
+Set-VMHost -VirtualHardDiskPath $def_vdPath
+
+#############################################################################################################################################
+#~ END ######################################################################################################################################
+#############################################################################################################################################
