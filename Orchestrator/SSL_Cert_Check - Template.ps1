8f5c2b9dd06478208e0821693ceb2628b75349df
Brian T <tanknospam@gmail.com>
2016-12-12 09:06:39 -0700

    Update SSL_Cert_Check - Template.ps1

--- Orchestrator/SSL_Cert_Check - Template.ps1
+++ Orchestrator/SSL_Cert_Check - Template.ps1
@@ -3,7 +3,7 @@
 #
 #~ Brian Tancredi
 #~ Created: 2016-12-08
-#~ Modified: 2016-12-09
+#~ Modified: 2016-12-12
 #
 #~ References:
 #~ Sourced from - https://iamoffthebus.wordpress.com/2014/02/04/powershell-to-get-remote-websites-ssl-certificate-expiration/
@@ -11,6 +11,7 @@
 #
 #~ Notes:
 #~ Write-Hosts commented out to work in Orchestrator otherwise; "Cannot invoke this function because the current host does not implement it."
+#~ Can uncomment if running locally.
 #
 #############################################################################################################################################
 
@@ -30,7 +31,7 @@ $urls = @(
 "https://www.site6.com")
 
  #~ Declare Functions
- function emailNotice{
+ function emailNotifyFail{
     $from = "email@email.com"
     $to = @("email@email.com")
     $cc =@(
@@ -45,19 +46,45 @@ $urls = @(
     Certificate Public Key: $certPublicKeyString
     Certificate Serial Number: $certSerialNumber
     Certificate Thumbprint: $certThumbprint
-    Cert issuer: $certIssuer
+    Certificate Issuer: $certIssuer
     Certificate Effective Date: $certEffectiveDate
     Certificate Expiration Date: $expiration"
     Send-MailMessage -from $from -to $to -Cc $cc -subject $subject -body $body -smtpserver $smtp
 } #Email Alert Settings and Configuration
 
-function localNoticePass{
+function localNotifyPass{
 #    Write-Host "Cert for site $url expires in $certExpiresIn days [on $expiration]`n" -ForegroundColor Green
 } #PowerShell Notifications on Pass
 
-function localNoticeFail{
+function localNotifyFail{
 #    Write-Host "$body `n`nThreshold is $minimumCertAgeDays days.`n" -ForegroundColor Yellow
 } #PowerShell Notifications on Fail
+
+function certProperties{
+     [datetime]$global:expiration = $req.ServicePoint.Certificate.GetExpirationDateString()
+     [int]$global:certExpiresIn = ($expiration - $(get-date)).Days
+     $global:certName = $req.ServicePoint.Certificate.GetName()
+     $global:certPublicKeyString = $req.ServicePoint.Certificate.GetPublicKeyString()
+     $global:certSerialNumber = $req.ServicePoint.Certificate.GetSerialNumberString()
+     $global:certThumbprint = $req.ServicePoint.Certificate.GetCertHashString()
+     $global:certEffectiveDate = $req.ServicePoint.Certificate.GetEffectiveDateString()
+     $global:certIssuer = $req.ServicePoint.Certificate.GetIssuerName()
+} #Decalre Certificate Properties
+
+function urlCheck{
+    Write-Host "Checking $url" -ForegroundColor Green
+    $global:req = [Net.HttpWebRequest]::Create($url)
+    $req.Timeout = $timeoutMilliseconds
+    try {$req.GetResponse() |Out-Null}
+    catch {#Write-Host "Exception while checking URL $url`: $_" -ForegroundColor Red
+    }
+} #URL Checks and Error Handling
+
+function cleanUp{
+    Remove-Variable req -Scope Script
+    Remove-Variable expiration -Scope Script
+    Remove-Variable certExpiresIn -Scope Script
+} #Remove stored Variables in Loop
 
 #############################################################################################################################################
 ###~ Get HTTP Response on URLs, find Expiring Certs, and send notification Emails ###########################################################
@@ -67,25 +94,11 @@ function localNoticeFail{
  [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 
 foreach ($url in $urls){
-#    Write-Host "Checking $url`n" -ForegroundColor Green
-    $req = [Net.HttpWebRequest]::Create($url)
-    $req.Timeout = $timeoutMilliseconds
-    try {$req.GetResponse() |Out-Null}
-    catch {#Write-Host "Exception while checking URL $url`: $_" -ForegroundColor Red
-    }
-    [datetime]$expiration = $req.ServicePoint.Certificate.GetExpirationDateString()
-    [int]$certExpiresIn = ($expiration - $(get-date)).Days
-    $certName = $req.ServicePoint.Certificate.GetName()
-    $certPublicKeyString = $req.ServicePoint.Certificate.GetPublicKeyString()
-    $certSerialNumber = $req.ServicePoint.Certificate.GetSerialNumberString()
-    $certThumbprint = $req.ServicePoint.Certificate.GetCertHashString()
-    $certEffectiveDate = $req.ServicePoint.Certificate.GetEffectiveDateString()
-    $certIssuer = $req.ServicePoint.Certificate.GetIssuerName()
-    If ($certExpiresIn -gt $minimumCertAgeDays){localNoticePass}
-    Else {emailNotice; localNoticeFail}
-    Remove-Variable req
-    Remove-Variable expiration
-    Remove-Variable certExpiresIn
+    urlCheck
+    certProperties
+    If ($certExpiresIn -gt $minimumCertAgeDays){localNotifyPass}
+    Else{emailNotifyFail; localNotifyFail}
+    cleanUp
 }
 
 #############################################################################################################################################
