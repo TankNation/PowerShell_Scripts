0f055fc3922f19f74d87ffa341a3fb7ead938e0c
Brian T <tanknospam@gmail.com>
2017-11-29 15:22:38 -0700

    Add files via upload

---
+++ Computer/Software/TimeZone Adjustment.ps1
@@ -0,0 +1,14 @@
+﻿
+Function Configure-TimeZone{
+    #~ Get current and Define desired Time Zones
+    $current_TZ = ([System.TimeZoneInfo]::Local).Id
+    $desired_TZ = "US Mountain Standard Time"
+
+    #~ Set Time Zone if not desired
+    If($current_TZ -ne $desired_TZ){
+        Write-Host "Adjusting Time Zone to $desired_TZ" -ForegroundColor Yellow
+        Set-TimeZone -Name $desired_TZ
+    }
+}#~ Configures systems set Time Zone
+
+Configure-TimeZone
